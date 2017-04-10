f1eefeec6e618dd23bac6107f03af495b07f5cf7
boom-e-rang <Ola@dhcp-10-105-87-192.wireless.northwestern.private>
2017-04-10 12:53:37 -0500

    HW4 continued

--- HW4/spi_example.c
+++ HW4/spi_example.c
@@ -1,6 +1,4 @@
 
-#include "NU32.h"       // constants, funcs for startup and UART
-
 // Demonstrates SPI
 // PIC is the master, DAC is the slave
 // Uses microchip MCP4902 chip 
@@ -22,21 +20,58 @@
 // SDO4 -> SI (pin F5 -> pin 5)
 // SDI4 -> SO (pin F4 -> pin 2)
 
-
-
 #define CS LATBbits.LATB8       // chip select pin
 
-// send a byte via spi and return the response
-unsigned char spi_io(unsigned char o) {
-  SPI2BUF = o;
-  while(!SPI2STATbits.SPIRBF) { // wait to receive the byte
+unsigned char spi_io(unsigned char o);
+void initSPI1(void);
+void ram_write(unsigned short addr, const char data[], int len);
+void ram_read(unsigned short addr, char data[], int len);
+void makeWaveform(void);
+
+
+int main(void) {
+  unsigned short addr1 = 0x1234;                  // the address for writing the ram
+  char data[] = "Help, I'm stuck in the RAM!";    // the test message
+  char read[] = "***************************";    // buffer for reading from ram
+  char buf[100];                                  // buffer for comm. with the user
+  unsigned char status;                           // used to verify we set the status 
+  
+  initSPI1(); 
+
+  // check the ram status
+  CS = 0;
+  spi_io(0x5);                                      // ram read status command
+  status = spi_io(0);                               // the actual status
+  CS = 1;
+
+  sprintf(buf, "Status 0x%x\r\n",status);
+  NU32_WriteUART3(buf);
+
+  sprintf(buf,"Writing \"%s\" to ram at address 0x%x\r\n", data, addr1);
+  NU32_WriteUART3(buf);
+                                                    // write the data to the ram
+  ram_write(addr1, data, strlen(data) + 1);         // +1, to send the '\0' character
+  ram_read(addr1, read, strlen(data) + 1);          // read the data back
+  sprintf(buf,"Read \"%s\" from ram at address 0x%x\r\n", read, addr1);
+  NU32_WriteUART3(buf);
+
+  while(1) {
     ;
   }
-  return SPI2BUF;
+  return 0;
 }
 
-// initialize SPI2
-void DAC_init() {
+
+unsigned char spi_io(unsigned char o) {
+  SPI1BUF = o;
+  while(!SPI1STATbits.SPIRBF) { // wait to receive the byte
+    ;
+  }
+  return SPI1BUF;
+}
+
+// initialize SPI1
+void initSPI1(void) {
   // set up the chip select pin as an output
   // the chip select pin is used by the slave to indicate
   // when a command is beginning (clear CS to low) and when it
@@ -44,18 +79,18 @@ void DAC_init() {
   TRISBbits.TRISB8 = 0;
   CS = 1;
 
-  // Master - SPI2, pins are: SDI4(F4), SDO4(F5), SCK4(F13).  
+  // Master - SPI1, pins are: SDI4(F4), SDO4(F5), SCK4(F13).  
   // we manually control SS4 as a digital output (F12)
   // since the pic is just starting, we know that SPI is off. We rely on defaults here
  
-  // setup SPI2
-  SPI2CON = 0;              // turn off the spi module and reset it
-  SPI2BUF;                  // clear the rx buffer by reading from it
-  SPI2BRG = 0x3;            // baud rate to 10 MHz [SPI2BRG = (80000000/(2*desired))-1]
-  SPI2STATbits.SPIROV = 0;  // clear the overflow bit
-  SPI2CONbits.CKE = 1;      // data changes when clock goes from hi to lo (since CKP is 0)
-  SPI2CONbits.MSTEN = 1;    // master operation
-  SPI2CONbits.ON = 1;       // turn on SPI2
+  // setup SPI1
+  SPI1CON = 0;              // turn off the spi module and reset it
+  SPI1BUF;                  // clear the rx buffer by reading from it
+  SPI1BRG = 0x3;            // baud rate to 10 MHz [SPI2BRG = (80000000/(2*desired))-1]
+  SPI1STATbits.SPIROV = 0;  // clear the overflow bit
+  SPI1CONbits.CKE = 1;      // data changes when clock goes from hi to lo (since CKP is 0)
+  SPI1CONbits.MSTEN = 1;    // master operation
+  SPI1CONbits.ON = 1;       // turn on SPI2
 
                             // send a ram set status command.
   CS = 0;                   // enable the ram
@@ -90,34 +125,13 @@ void ram_read(unsigned short addr, char data[], int len) {
   CS = 1;
 }
 
-int main(void) {
-  unsigned short addr1 = 0x1234;                  // the address for writing the ram
-  char data[] = "Help, I'm stuck in the RAM!";    // the test message
-  char read[] = "***************************";    // buffer for reading from ram
-  char buf[100];                                  // buffer for comm. with the user
-  unsigned char status;                           // used to verify we set the status 
-  NU32_Startup();   // cache on, interrupts on, LED/button init, UART init
-  ram_init(); 
-
-  // check the ram status
-  CS = 0;
-  spi_io(0x5);                                      // ram read status command
-  status = spi_io(0);                               // the actual status
-  CS = 1;
-
-  sprintf(buf, "Status 0x%x\r\n",status);
-  NU32_WriteUART3(buf);
-
-  sprintf(buf,"Writing \"%s\" to ram at address 0x%x\r\n", data, addr1);
-  NU32_WriteUART3(buf);
-                                                    // write the data to the ram
-  ram_write(addr1, data, strlen(data) + 1);         // +1, to send the '\0' character
-  ram_read(addr1, read, strlen(data) + 1);          // read the data back
-  sprintf(buf,"Read \"%s\" from ram at address 0x%x\r\n", read, addr1);
-  NU32_WriteUART3(buf);
-
-  while(1) {
-    ;
+void makeWaveform(void) {
+  int i = 0, center = 500, A = 300;     // square wave, fill in center value and amplitude
+  for (i = 0; i < NUMSAMPS; ++i) {
+    if (i < NUMSAMPS/2) {
+      Waveform[i] = center + A;
+    } else {
+      Waveform[i] = center - A;
+    }
   }
-  return 0;
 }
